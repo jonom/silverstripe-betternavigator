747a437a24346a2a9faa7b8b02a25258820b34e9
Jono Menz <jonomenz@gmail.com>
2018-04-06 15:38:18 -0700

    Restore false

--- src/Extension/BetterNavigatorExtension.php
+++ src/Extension/BetterNavigatorExtension.php
@@ -51,10 +51,9 @@ class BetterNavigatorExtension extends DataExtension {
                 }
             }
             // Only show edit link if user has permission to edit this page
-			$editLink = array_key_exists('CMSLink', $nav)
-				&& ($isDev || $this->owner->dataRecord->canEdit() && Permission::check('CMS_ACCESS_CMSMain'))
-                ? $nav['CMSLink']['Link']
-                : '';
+            $editLink = array_key_exists('CMSLink', $nav)
+                && ($isDev || $this->owner->dataRecord->canEdit() && Permission::check('CMS_ACCESS_CMSMain'))
+                ? $nav['CMSLink']['Link'] : false;
 
             // Is the logged in member nominated as a developer?
             $member = Member::currentUser();
